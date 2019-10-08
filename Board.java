e358ebe1a32d3348a29990a71e6317d822bbe5f8
ryanjung98 <56048146+ryanjung98@users.noreply.github.com>
2019-10-08 16:59:57 -0600

    Update Board.java

--- Board.java
+++ Board.java
@@ -2,4 +2,6 @@ import java.util.ArrayList;
 public class Board {
 // Update board from piece movement 
 // Instance variables needed: ArrayList
+  private ArrayList<Piece> pieces= new ArrayList<Piece>();
+  
 }
