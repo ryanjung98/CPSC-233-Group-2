92fdbc3ecd0d9b1f9ceb2367f9a02934a18e717a
ryanjung98 <56048146+ryanjung98@users.noreply.github.com>
2019-10-08 17:18:29 -0600

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -3,5 +3,13 @@ public class Pawn extends Piece{
 // INSTANCE VARIABLES
 private int xpos;
 private int ypos;
- 
+//CONSTRUCTORS
+public Pawn (int x, int y){
+this.xpos = x;
+this.ypos = y;
+}
+public Pawn (Pawn p){
+this.xpos = p.xpos;
+this.ypos = p.ypos;
+}
 }
