7fd1649843b70f6fb18bc4995ec42712fc8f580b
ryanjung98 <56048146+ryanjung98@users.noreply.github.com>
2019-10-08 17:13:08 -0600

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,4 +1,20 @@
 
 public class Piece {
-
+  //INSTANCE VARIABLES
+  private boolean white; //boolean for if white or black
+  private int xpos; private int ypos; //positions of piece between 1 and 8
+  private boolean danger;
+  
+  //CONSTRUCTORS
+  public Piece(boolean white, boolean danger){
+    this.white=white;
+    this.danger=danger;
+  }
+  public Piece(Piece shithead){
+    white=shithead.white;
+    danger=shithead.danger;
+    xpos=shithead.xpos;
+    ypos=shithead.ypos;
+  }
+  
 }
