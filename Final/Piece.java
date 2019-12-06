a3f56e318b69864d5b29c95a136e91f0547c7d7e
mohamedmohamed1 <52938358+mohamedmohamed1@users.noreply.github.com>
2019-12-06 15:22:02 -0700

    Update Piece.java

--- Final/Piece.java
+++ Final/Piece.java
@@ -1,74 +1,84 @@
+/*
+ * The Piece class is the Parent class for all the types of Chess Pieces 
+*/
+
+
 import java.lang.Math;
-public abstract class Piece {//Parent class for all the types of chess pieces
+public abstract class Piece {
+	
   //INSTANCE VARIABLES
-  private boolean white; //boolean for if white or black
-  private int xpos; private int ypos; //positions of piece between 1 and 8
-  private char piece; //character used to represent the piece in text based game
-  private boolean hasMoved; //indicates if the piece has moved yet
+	
+  public boolean white; //boolean for if white or black
+  public int xpos; public int ypos; //positions of piece between 1 and 8
+  public boolean danger;
+  public char piece;
+  public boolean hasMoved;
   
   //CONSTRUCTORS
+  
   public Piece(char p){
    this.piece = p;
   }
-  public Piece(Piece p){//copy constructor
+  public Piece(Piece p){
    this.piece = p.piece;
   }
-  public Piece(int x, int y, boolean w){//constructor taking x and y positions boolean indicating if the piece is white
+  public Piece(int x, int y, boolean w){
 	  this.xpos=x;
 	  this.ypos=y;
 	  this.white=w;
 	  this.hasMoved=false;
 	  }
-	  public Piece(int x, int y, boolean w, boolean h){//constructor does the same as above but also considers if the piece has moved
+	  public Piece(int x, int y, boolean w, boolean h){
 	  this.xpos=x;
 	  this.ypos=y;
 	  this.white=w;
 	  this.hasMoved=h;
 	  }
 	  
-	public Piece(){//default constructor
+	public Piece(){
+		
 		}
 
 	  
-//GETTER METHODS
+//getter methods
 
-public abstract int getValue();//will return the value of the piece. Used for the AI
+public abstract int getValue();
 
-public abstract boolean canMove(int i, int j);//This method will contain the logic required to determine if the piece can be moved to the spot i,j from it's current position
-public abstract void setEnPassant(boolean boo);//This method is used to set whether or not a piece can be taken by En Passant
-public abstract boolean getEnPassant();//Checks if a piece can be taken by En Passant
+public abstract boolean canMove(int i, int j);
+public abstract void setEnPassant(boolean boo);//had to also make useless overide methods for all the pieces
+public abstract boolean getEnPassant();//had to also make useless overide methods for all the pieces
 
-public boolean getW(){//Returns if the piece is white
+public boolean getW(){
 	return this.white;
 	}
-	public char getP(){//Returns the character representing the type of the piece
+	public char getP(){
 		return this.piece;
 		}
-public int getX(){//Returns X position
+public int getX(){
 	return this.xpos;
 	}  
 	
-public int getY(){//Returns Y position
+public int getY(){
 	return this.ypos;
 	}
-public boolean getHasMoved(){//Returns if the piece has moved or not
+public boolean getHasMoved(){
 	return this.hasMoved;
 }
-//SETTER METHODS	
-public void setW(boolean t){//Sets color of the piece
+//setter methods	
+public void setW(boolean t){
 	this.white=t;
 	}
-public void setP(char p){//Sets character of the piece
+public void setP(char p){
 	this.piece = p;
 	
 	}
-public void setX(int x){//Sets X position
+public void setX(int x){
 	this.xpos = x;
 	}
-public void setY(int y){//Sets Y position
+public void setY(int y){
 	this.ypos = y;
 	}
-public void setHasMoved(boolean hasMoved){//Sets if the piece has moved or not
+public void setHasMoved(boolean hasMoved){
 	this.hasMoved=hasMoved;
 }
 }
